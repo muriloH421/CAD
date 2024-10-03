081f25928f3f93cd77d94c2c5709efc66571b498
murilo <basiliomurilo421@gmail.com>
2024-10-03 08:48:42 -0300

    .....

---
+++ src/_codux/boards/new-board.board.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { createBoard } from "@wixc3/react-board";
+
+export default createBoard({
+  name: "New Board",
+  Board: () => (
+    <div>
+      <nav>
+        <a href="/home">Home</a> | <a href="/projects">Projects</a> |{" "}
+        <a href="/about">About</a> | <a href="/contact">Contact Us</a>
+      </nav>
+    </div>
+  ),
+  isSnippet: true,
+});
