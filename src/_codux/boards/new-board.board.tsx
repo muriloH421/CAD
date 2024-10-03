1d3cec89632aabb6c8ba360de46b2e4b876fc4ad
murilo <basiliomurilo421@gmail.com>
2024-10-03 09:09:50 -0300

    tste

--- src/_codux/boards/new-board.board.tsx
+++ src/_codux/boards/new-board.board.tsx
@@ -1,3 +1,4 @@
+import "./new-board.board.css";
 import React from "react";
 import { createBoard } from "@wixc3/react-board";
 
@@ -5,10 +6,15 @@ export default createBoard({
   name: "New Board",
   Board: () => (
     <div>
-      <nav>
-        <a href="/home">Home</a> | <a href="/projects">Projects</a> |{" "}
+      <div />
+      <nav className="NewBoardBoard_nav1">
+        <a href="/home">Sobre Nós</a> | <a href="/projects">Projects</a> |{" "}
         <a href="/about">About</a> | <a href="/contact">Contact Us</a>
       </nav>
+      <img
+        src="https://wixplosives.github.io/codux-assets-storage/add-panel/image-placeholder.jpg"
+        alt=""
+      />
     </div>
   ),
   isSnippet: true,
