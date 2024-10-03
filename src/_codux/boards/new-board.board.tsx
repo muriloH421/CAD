b376a4d86d0d937d18851ba49ea506632b9d6629
murilo <basiliomurilo421@gmail.com>
2024-10-03 14:21:59 -0300

    nav bar pronta

--- src/_codux/boards/new-board.board.tsx
+++ src/_codux/boards/new-board.board.tsx
@@ -2,17 +2,27 @@ import "./new-board.board.css";
 import React from "react";
 import { createBoard } from "@wixc3/react-board";
 import AQuadroBrancoCanvaGoogleChrome0208202416241112Jpg from "../../assets/a - quadro branco - canva - google chrome 02_08_2024 16_24_11 1 (2).jpg";
+import VectorSvg from "../../assets/Vector.svg";
 
 export default createBoard({
   name: "New Board",
   Board: () => (
     <div className="NewBoardBoard_div1">
-      <div></div>
       <nav className="NewBoardBoard_nav1">
-        <a href="/home">Sobre Nós</a>
-        <a href="/about">About</a> <a href="/contact">Contact Us</a>
+        <a href="/home" className="NewBoardBoard_a3">
+          tipos de deficiencia
+        </a>
+        <a href="/home" className="NewBoardBoard_a4">
+          centro de ajuda
+        </a>
+        <a href="/home" className="NewBoardBoard_a5">
+          Sobre Nós
+        </a>
+        <a href="/about" className="NewBoardBoard_a6">
+          colaborar
+        </a>{" "}
+        <img src={VectorSvg} />
       </nav>
-      <a href="/home" className="NewBoardBoard_a2"></a>
       <img
         src={AQuadroBrancoCanvaGoogleChrome0208202416241112Jpg}
         alt=""
@@ -21,4 +31,7 @@ export default createBoard({
     </div>
   ),
   isSnippet: true,
+  environmentProps: {
+    windowWidth: 1041,
+  },
 });
